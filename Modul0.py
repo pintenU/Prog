9168f553d79cd983403cee1142bd509678956735
your_pintanU <your_alexanderpinter@telia.com>
2024-08-26 14:27:18 +0200

    Uppgift 1

---
+++ Modul0.py
@@ -0,0 +1,20 @@
+#välkommen till python
+
+"""hello everyone
+detta är ett block av kommentarer"""
+
+
+print("hello world")
+
+#string/sträng variabel
+name = "Alexander"
+print(name)
+
+#integer/nummer variabel
+my_age = 16
+
+print(my_age)
+
+print( name + " är " + str(my_age) + " år gammal " )
+
+#Float 
---
+++ Modul1.py
@@ -0,0 +1,28 @@
+print("Hello World")
+
+print("Anakin said: \"I am your father\" ")
+
+print("""
+      jag är bäst
+jag är cool 
+      jawg är programmerig
+      """)
+
+print("""
+Drottning blanka
+  Drottning blanka
+    Drottning blanka
+      Drottning blanka
+       Drottning blanka
+        Drottning blanka
+         Drottning blanka
+          Drottning blanka
+           Drottning blanka
+            Drottning blanka""")
+
+
+name = input("State your name:"  )
+
+print("välkommen \"" + name + "\"" )
+
+print("Alexander\nPintér")
