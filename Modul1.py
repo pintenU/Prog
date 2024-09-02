6296bd250ac2ec37e94bb19ca0d7df3325107afd
your_pintanU <your_alexanderpinter@telia.com>
2024-09-02 14:44:30 +0200

    Klar Uppgift2

--- Modul1.py
+++ Modul1.py
@@ -21,8 +21,50 @@ Drottning blanka
             Drottning blanka""")
 
 
-name = input("State your name:"  )
 
+
+
+
+"""
 print("välkommen \"" + name + "\"" )
 
 print("Alexander\nPintér")
+"""
+"""name = input("state your name:")
+
+age = input("State your age:")
+
+
+print("Welcome:",name,"you are",age,"Years old")
+
+
+
+x = input("x: ")
+y = input("y: ")
+
+
+print("Svar:", int(x) * int(y))   # <-- miniräknare som multiplicerar
+
+
+length = input("Längd:")
+weight = input("vikt:")
+
+print("Ditt BMI är:", float(weight) / float(length)**2  )
+
+
+
+age = input("Hur gammal är du?:")
+
+weeks = int(age) * int(52)
+
+
+print("Fun Fact du är ungefär", weeks, "veckor gammal" )
+"""
+
+""" 2.20462262185 """
+
+kg = float(input("Hur många kilo väger du?:"))
+
+lbs = kg * 2.20462262185
+
+print("fun fact: Du väger", lbs, "lbs" )
---
+++ Modul2.py
@@ -0,0 +1,71 @@
+
+#MODUL 2 - VARIABLER
+
+
+"""¨
+DATATYPER I PYTHON
+Strång, string = "text"
+heltal, integer = 1... 2... 3... 10
+flyttal, float = 1.2    Skriv alltid flyttal med punkt som decimal
+boolesk, boolean = True/False   flagga eller av och på
+
+
+
+deklarera / initiera
+
+let value; #deklarera
+
+"""
+
+#initiera  (namn på variabel samt datatyp)
+name = "" #string
+value = 1.3 #float
+value2 = 8 #integer
+value3 = 8.1 #float
+check = False #boolean
+
+
+x = 9
+y = 10
+
+"""
+sum = x + y 
+sum = x - y
+sum = x * y
+sum = x / y 
+"""
+
+
+import os 
+os.system("cls")
+
+print (sum)
+
+x = input("x: ") #input är strång från början - du måste typkonvertera innan du gör beräkningar
+y = input("y: ")
+
+
+print("svar:", int(x) + int(y))
+
+
+print("Svar:", int(x) - int(y))
+
+
+print("svar:", int(x) * int(y))
+
+
+print("svar:", int(x) / int(y))
+
+
+print("svar:", int(x) ** int(y))
+
+
+print("svar:", int(x) // int(y)) 
+
+
+# upphöjt görs med **
+# modulo görs med % och visar resten med en division
+# för att ta bort decimaler //
+
+
+
