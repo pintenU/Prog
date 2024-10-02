ddda36bb15937b7645244546518591aa517e2662
your_pintanU <your_alexanderpinter@telia.com>
2024-10-02 12:30:22 +0200

    Modul 4 klar

---
+++ Modul4.py
@@ -0,0 +1,63 @@
+import os
+os.system("cls")
+
+# number = int(input("Hur många nummer vill du ha? "))
+
+
+# while True:
+    
+#     letter = input("Vill du lämna? Skriv q: ")
+#     print(letter)
+#     if letter.upper()=="Q":
+#         break
+
+
+
+# repeat = input("Vad vill du ska skrivas 10 ggr? ")
+
+# for i in range(1,11):
+#     print(repeat)
+    
+
+    
+# for i in range(1,11):
+#     print(i)
+
+
+
+# number = int(input("vilket nummer? "))
+
+
+# for i in range(1, number + 1):
+#     print (i)
+    
+    
+
+
+
+
+
+
+     
+               
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
