6a9a225916898618a30584f2c9772bc569e28b15
your_pintanU <your_alexanderpinter@telia.com>
2024-11-27 11:44:37 +0100

    modul5 klar

---
+++ Modul5.py
@@ -0,0 +1,36 @@
+import os
+from random import randint
+
+os.system("cls")
+
+# my_num = 0
+# while True:
+#     try:
+#         my_num = int(input("skriv ett nummer: "))
+#     except:
+#         print("skriv nummer")
+
+
+
+# text = input("skriv ett ord ")
+
+# print(text[:10])
+
+
+text = input("skriv ett ord ")
+
+print(f"{text[:10]}...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Problemlösning2.py
@@ -0,0 +1,20 @@
+import os
+os.system("cls")
+
+
+
+
+
+cars = ["Porsche", "Ferrari", "Lambo"]
+
+x = 1
+
+for car in cars:
+    print(f"{x})  {car}")
+    x += 1
+
+
+
+
+
+
