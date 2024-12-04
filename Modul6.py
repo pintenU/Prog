aa3a9dbaf8bb6cedd2ee4d6779b3c765ed554771
your_pintanU <your_alexanderpinter@telia.com>
2024-12-04 12:18:42 +0100

    uppgift6 klar

---
+++ Modul6.py
@@ -0,0 +1,60 @@
+import os
+os.system("cls")
+
+
+
+# list = ["bananar" , "wooooooow", "Secy ladt", "Bazinga"]
+
+# for i in list:
+#     print(i)
+
+
+
+# names = ["Adolf"]
+
+# names[0] = "Bitches"
+
+# for i in names:
+#     print(i)
+
+
+
+
+# names = ["Adolf"]
+# names.append("Crazy")
+
+# for i in names:
+#     print(i)
+
+
+
+# names = ["Adolf" , "wow" , "Scroumpcious" , "Rappa" , "Crazy"]
+
+# print(len(names))
+
+
+
+
+# names = ["Adolf" , "Wowzers" , "Karma"]
+# names.pop(0)
+
+
+# for i in names:
+#     print(i)
+
+
+# names = ["Adolf" , "Wowzers" , "Karma"]
+
+# while True:
+#     names.append(input("Skriv in ditt namn: "))
+#     for i in names:
+#         print(i)
+    
+#     delete = input("ta bort npn? ")
+#     names.pop(int(delete))
+
+
+
+
+
+
